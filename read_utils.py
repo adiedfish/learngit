15a1537d38780b6582b7df29858995eb6fdf7f68
李腾辉 <litenghui@litenghuideMacBook-Pro.local>
2018-07-19 09:37:21 +0800

    modified read_utils

--- read_utils.py
+++ read_utils.py
@@ -50,7 +50,7 @@ def pre_process(filname):
 					if cout_sumn%100000 == 0:
 						pre = float(cout)/float(sumn)
 						pre_sumn = float(cout_sumn)/float(sumn)
-						sys.stdout.write("%.4f%      %.4f%"%(pre, pre_sumn))
+						sys.stdout.write("%.4f%%______________%.4f%%"%(pre, pre_sumn))
 						sys.stdout.write("\r")
 						sys.stdout.flush()
 		finally:
