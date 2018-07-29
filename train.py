e42bc7ac17cfdca202265ad99684d87639133ee6
李腾辉 <litenghui@litenghuideMacBook-Pro.local>
2018-07-29 17:17:20 +0800

    change z1 and z2

--- train.py
+++ train.py
@@ -88,7 +88,7 @@ w1 = tf.Variable(tf.random_uniform(w1_shape, minval=-init_range, maxval=init_ran
 
 b1 = tf.Variable(tf.zeros(b1_shape,dtype=tf.float32))
 
-z1 = tf.sparse_tensor_dense_matmul(tf.sparse_tensor_dense_matmul(support_2,tf.matmul(x, w1)))
+z1 = tf.sparse_tensor_dense_matmul(support,tf.sparse_tensor_dense_matmul(support_2,tf.matmul(x, w1)))
 
 activate = tf.nn.relu(z1+b1)
 
@@ -100,7 +100,7 @@ w2 = tf.Variable(tf.random_uniform(w2_shape, minval=-init_range, maxval=init_ran
 
 b2 = tf.Variable(tf.zeros(b2_shape,dtype=tf.float32))
 
-z2 = tf.sparse_tensor_dense_matmul(tf.sparse_tensor_dense_matmul(support_2,tf.matmul(activate, w2)))
+z2 = tf.sparse_tensor_dense_matmul(support,tf.sparse_tensor_dense_matmul(support_2,tf.matmul(activate, w2)))
 #这里后面反倒用matmul(z1,w2)更好
 
 predict = tf.nn.softmax(z2+b2)
