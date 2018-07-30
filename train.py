b9f8686e159171030c9a75e230d86c068b62c089
李腾辉 <litenghui@litenghuideMacBook-Pro.local>
2018-07-30 13:39:02 +0800

    cahnge train.py

--- train.py
+++ train.py
@@ -54,6 +54,7 @@ features_save_filename = "n_features_martix_without_port"
 labels_save_filename = "labers"
 labels_for_test_save_filename = "labels_for_test"
 
+j = 0
 with open(features_save_filename,'r') as f:
 	features = pkl.load(f)
 	print("features load done")
@@ -61,11 +62,11 @@ with open(features_save_filename,'r') as f:
 with open(labels_save_filename,'r') as f:
 	labels_all = pkl.load(f)
 	print("labels load done")
-'''
-with open(labels_for_test_save_filename,'r') as f:
+
+with open("fin_pre/"+labels_for_test_save_filename+str(j),'r') as f:
 	labels_for_test = pkl.load(f)
 	print("labels for test load done")
-'''
+
 with open(sparse_save_filename,'r') as f:
 	sparse = pkl.load(f)
 	print("sparse load done")
@@ -136,106 +137,92 @@ with open("test_num",'r') as f:
 	test_num = pkl.load(f)
 
 
-for j in range(18):
-	with open("fin_pre/"+labels_for_test_save_filename+str(j),'r') as f:
-		labels_for_test = pkl.load(f)
-		print("labels for test load done")
+for i in range(epochs):
+	#t = time.time()
 
-	for i in range(epochs):
-		#t = time.time()
-
-		sess.run(train_step,feed_dict={support:sparse_martix,x:features,labels:labels_for_test})
-		'''
-		train_loss = sess.run(loss, feed_dict={support:sparse_martix,x:features,labels:labels_for_test})
-		train_acc_tf = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(predict,1),tf.argmax(labels_for_test,1)),"float"))
-		train_acc = sess.run(train_acc_tf,feed_dict={support:sparse_martix,x:features,labels:labels_for_test})
-		
-		
-		test_loss = sess.run(loss, feed_dict={support:sparse_martix,x:features,labels:labels_all})
-		test_acc_tf = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(predict,1),tf.argmax(labels_for_test,1)),"float"))
-		test_acc = sess.run(test_acc_tf,feed_dict={support:sparse_martix,x:features,labels:labels_all})
-		
-		
-		print("Epoch:",'%04d'%(i+1)," train_loss=","{}".format(train_loss),
-			"train_acc=","{}".format(train_acc),"test_loss=","{}".format(test_loss),
-			"test_acc=","{}".format(test_acc),"time=","{}".format(time.time()-t))
-		'''
-		#print("Epoch: %04d"%(i+1))
-		allb = 0
-		cout = 0
-		v = sess.run(predict,feed_dict={support:sparse_martix,x:features})
-		ind_all = sess.run(tf.argmax(v,1))
-		for i in xrange(len(labels_all)):
-			if labels_all[i][1] == 1:
-				allb += 1
-				if ind_all[i] == 1:
-					cout += 1
-		print("how much we predict right: %d/  %d"%(cout-test_num[1], allb-test_num[1]))
-		if cout - test_num[1] >0:
-			rec = (float(cout)-float(test_num[1]))/(float(allb)-float(test_num[1]))
-		else:
-			rec = 0
-		print("blacklist predict pro:%.4f"%(rec))
-
-		allb = 0
-		cout = 0
-		for i in xrange(len(labels_all)):
-			#ind = sess.run(tf.argmax(v[i]))
-			if ind_all[i] == 1:
-				allb += 1
-				if labels_all[i][1] == 1:
-					cout += 1
-		print("how much we predict: %d/  %d"%(allb, cout-test_num[1]))
-		if cout - test_num[1] >0:
-			acc = (float(cout)-float(test_num[1]))/(float(allb)+1)
-		else:
-			acc = 0
-		print("blacklist predict pro:%.4f"%(acc))
-		
-		if rec+acc != 0:
-			f1_soc = 2*(rec*acc)/(rec+acc)
-		else:
-			f1_soc = 0
-		if f1_soc > max_f1:
-			max_f1 = f1_soc
-		print("%.5f       %d"%(f1_soc,j))
-		print("-------------------------------------")
-
-	print("w1 :  ---\n")
-	w_1 = sess.run(w1)
-	print(w_1)
-	print("------------------------------------")
-	print("w2 :  ---\n")
-	w_2 = sess.run(w2)
-	print(w_2)
-	print("Optimization Finished")
-	print("max f1:%.4f"%max_f1)
-
-	with open("fin_pre/fin_w1_"+str(j),'w+') as f:
-		pkl.dump(w_1,f)
-	b_1 = sess.run(b1)
-	with open("fin_pre/fin_b1_"+str(j),'w+') as f:
-		pkl.dump(b_1,f)
-
-	with open("fin_pre/fin_w2_"+str(j),'w+') as f:
-		pkl.dump(w_2,f)
-	b_2 = sess.run(b2)
-	with open("fin_pre/fin_b2_"+str(j),'w+') as f:
-		pkl.dump(b_2,f)
-
-	w1 = tf.random_uniform(w1_shape, minval=-init_range, maxval=init_range, dtype=tf.float32)
-
-	b1 = tf.zeros(b1_shape,dtype=tf.float32)
-
-	w2 = tf.random_uniform(w2_shape, minval=-init_range, maxval=init_range, dtype=tf.float32)
-
-	b2 = tf.zeros(b2_shape,dtype=tf.float32)
+	sess.run(train_step,feed_dict={support:sparse_martix,x:features,labels:labels_for_test})
+	'''
+	train_loss = sess.run(loss, feed_dict={support:sparse_martix,x:features,labels:labels_for_test})
+	train_acc_tf = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(predict,1),tf.argmax(labels_for_test,1)),"float"))
+	train_acc = sess.run(train_acc_tf,feed_dict={support:sparse_martix,x:features,labels:labels_for_test})
 	
-	fin_pre = sess.run(predict,feed_dict={support:sparse_martix,x:features})
-	fin_pre = sess.run(tf.argmax(fin_pre,1))
+	
+	test_loss = sess.run(loss, feed_dict={support:sparse_martix,x:features,labels:labels_all})
+	test_acc_tf = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(predict,1),tf.argmax(labels_for_test,1)),"float"))
+	test_acc = sess.run(test_acc_tf,feed_dict={support:sparse_martix,x:features,labels:labels_all})
+	
+	
+	print("Epoch:",'%04d'%(i+1)," train_loss=","{}".format(train_loss),
+		"train_acc=","{}".format(train_acc),"test_loss=","{}".format(test_loss),
+		"test_acc=","{}".format(test_acc),"time=","{}".format(time.time()-t))
+	'''
+	#print("Epoch: %04d"%(i+1))
+	allb = 0
+	cout = 0
+	v = sess.run(predict,feed_dict={support:sparse_martix,x:features})
+	ind_all = sess.run(tf.argmax(v,1))
+	for i in xrange(len(labels_all)):
+		if labels_all[i][1] == 1:
+			allb += 1
+			if ind_all[i] == 1:
+				cout += 1
+	print("how much we predict right: %d/  %d"%(cout-test_num[1], allb-test_num[1]))
+	if cout - test_num[1] >0:
+		rec = (float(cout)-float(test_num[1]))/(float(allb)-float(test_num[1]))
+	else:
+		rec = 0
+	print("blacklist predict pro:%.4f"%(rec))
 
-	with open("fin_pre/fin_pre_"+str(j),'w+') as f:
-		pkl.dump(fin_pre,f)
+	allb = 0
+	cout = 0
+	for i in xrange(len(labels_all)):
+		#ind = sess.run(tf.argmax(v[i]))
+		if ind_all[i] == 1:
+			allb += 1
+			if labels_all[i][1] == 1:
+				cout += 1
+	print("how much we predict: %d/  %d"%(allb, cout-test_num[1]))
+	if cout - test_num[1] >0:
+		acc = (float(cout)-float(test_num[1]))/(float(allb)+1)
+	else:
+		acc = 0
+	print("blacklist predict pro:%.4f"%(acc))
+	
+	if rec+acc != 0:
+		f1_soc = 2*(rec*acc)/(rec+acc)
+	else:
+		f1_soc = 0
+	if f1_soc > max_f1:
+		max_f1 = f1_soc
+	print("%.5f       %d"%(f1_soc,j))
+	print("-------------------------------------")
+
+print("w1 :  ---\n")
+w_1 = sess.run(w1)
+print(w_1)
+print("------------------------------------")
+print("w2 :  ---\n")
+w_2 = sess.run(w2)
+print(w_2)
+print("Optimization Finished")
+print("max f1:%.4f"%max_f1)
+
+with open("fin_pre/fin_w1_"+str(j),'w+') as f:
+	pkl.dump(w_1,f)
+b_1 = sess.run(b1)
+with open("fin_pre/fin_b1_"+str(j),'w+') as f:
+	pkl.dump(b_1,f)
+with open("fin_pre/fin_w2_"+str(j),'w+') as f:
+	pkl.dump(w_2,f)
+b_2 = sess.run(b2)
+with open("fin_pre/fin_b2_"+str(j),'w+') as f:
+	pkl.dump(b_2,f)
+	
+fin_pre = sess.run(predict,feed_dict={support:sparse_martix,x:features})
+fin_pre = sess.run(tf.argmax(fin_pre,1))
+
+with open("fin_pre/fin_pre_"+str(j),'w+') as f:
+	pkl.dump(fin_pre,f)
 
 
 
